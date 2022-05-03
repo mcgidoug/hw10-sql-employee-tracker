41db390a3dd6d759d293af94b8edb1f546ce7fb0
mcgidoug <mcgidoug@gmail.com>
2022-05-02 20:37:51 -0500

    initial commit

---
+++ schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+USE employees_db;
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE role (
+    ID int AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 0) NOT NULL,
+    department_id INT,
+    PRIMARY KEY (id)
+);
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
---
+++ seeds.sql
@@ -0,0 +1,33 @@
+-- employees
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, 1);
+-- deptartments
+INSERT INTO department (department_name)
+VALUES ('Salesperson');
+INSERT INTO department (department_name)
+VALUES ('Salesperson');
+INSERT INTO department (department_name)
+VALUES ('Salesperson');
+INSERT INTO department (department_name)
+VALUES ('Salesperson');
+INSERT INTO department (department_name)
+VALUES ('Salesperson');
+-- roles
+INSERT INTO role (title, salary, department_id)
+VALUES ('salesperson', 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ('salesperson', 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ('salesperson', 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ('salesperson', 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ('salesperson', 100000, 1);
